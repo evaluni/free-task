aa5a088daea800016f2b1f2333f4d8abc6e13dc6
rika-t <kenta_takahashi@dwango.co.jp>
2016-11-10 06:43:40 +0900

    add UserTable#findByName method

--- example/src/main/scala/com/evaluni/txn_example/domain/UserHandler.scala
+++ example/src/main/scala/com/evaluni/txn_example/domain/UserHandler.scala
@@ -2,6 +2,7 @@ package com.evaluni.txn_example.domain
 
 import com.evaluni.txn_example.domain.UserRepository.Create
 import com.evaluni.txn_example.domain.UserRepository.Find
+import com.evaluni.txn_example.domain.UserRepository.FindByName
 import com.evaluni.txn_example.domain.store.EntityOp
 import com.evaluni.txn_example.infra.EntityIOHandler
 import com.evaluni.txn_example.infra.SampleDatabase.IO
@@ -12,6 +13,9 @@ object UserHandler extends EntityIOHandler {
   override def handle[A](msg: EntityOp[A]): Option[IO[A]] = Some(msg).collect {
     case Find(id) =>
       UserTable.selectById(id)
+
+    case FindByName(name) =>
+      UserTable.selectByName(name)
 
     case Create(name, age) =>
       UserTable.insert(name, age)
